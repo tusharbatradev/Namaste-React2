10ea17f999d7925cffcbce05cf3c13b276b8645f
Tushar Batra <tusharbatra08@gmail.com>
2024-04-08 18:05:11 +0530

    Namaste React 1st Episode

---
+++ app.js
@@ -0,0 +1,27 @@
+// const heading = React.createElement("h1", { className: "heading" }, "Hello From React");
+
+
+const parent = React.createElement("div", { id: "parent" },
+    // Array of Parent Element
+    [
+        React.createElement(
+            "div", { id: "child" },
+            // Array of Chidrens
+            [
+                React.createElement("h1", {}, "Hello I am h1 Tag"),
+                React.createElement("h2", {}, "Hello I am h2 Tag"),
+            ]
+        ),
+        React.createElement(
+            "div", { id: "child2" },
+            // Array of Chidrens
+            [
+                React.createElement("h1", {}, "Hello I am h1 Tag"),
+                React.createElement("h2", {}, "Hello I am h2 Tag"),
+            ]
+        )
+    ]
+);
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
